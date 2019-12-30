3bdab2f632a0acfdc266c8320e112a51696d6d2f
nir_ya <nir.yazdi@mail.huji.ac.il>
2019-12-30 17:53:59 +0200

    added onClick method for login button

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String username, password, emailAddress;
+    EditText nameText, passwordText, emailText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nameText = findViewById(R.id.username_text);
+        passwordText = findViewById(R.id.password_text);
+        emailText = findViewById(R.id.email_text);
+    }
+
+
+    public void login(View view) {
+
+        // todo error handling
+        username = nameText.getText().toString();
+        password = passwordText.getText().toString();
+        emailAddress = emailText.getText().toString();
     }
 }
