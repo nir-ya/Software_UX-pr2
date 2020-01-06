5ff4a233043ba224438ba6130f7e4bbce3e7d750
Liav <liav25@gmail.com>
2020-01-06 19:32:15 +0200

    fix card colors

--- app/src/main/java/com/example/myapplication/OrderListItem.java
+++ app/src/main/java/com/example/myapplication/OrderListItem.java
@@ -3,17 +3,25 @@ package com.example.myapplication;
 
 
 public class OrderListItem {
+    private final static int MIN_ORDER = 70;
     private String orderTime;
+    private int currentPrice;
 
     public OrderListItem(){
-
     }
 
-    public OrderListItem(String orderTime){
+    public OrderListItem(String orderTime, int price){
+        this.currentPrice = price;
         this.orderTime = orderTime;
+    }
+
+    public int getCurrentPrice() {
+        return currentPrice;
     }
 
     public String getOrderTime() {
         return orderTime;
     }
+
+
 }
