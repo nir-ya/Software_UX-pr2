f0522b6eda35a360a12eb20a865069952156b143
Liav <liav25@gmail.com>
2020-01-07 17:08:14 +0200

    add locked mode all connected to server

--- app/src/main/java/com/example/myapplication/OrderListItem.java
+++ app/src/main/java/com/example/myapplication/OrderListItem.java
@@ -1,18 +1,19 @@
 package com.example.myapplication;
 
 
-
 public class OrderListItem {
     private final static int MIN_ORDER = 70;
     private String serial;
     private int price;
+    private String status;
 
-    public OrderListItem(){
+    public OrderListItem() {
     }
 
-    public OrderListItem(String serial, int price){
-        this.price = price;
-        this.serial = serial;
+    public OrderListItem(String mSerial, int mPrice, String mStatus) {
+        this.price = mPrice;
+        this.serial = mSerial;
+        this.status = mStatus;
     }
 
     public int getPrice() {
@@ -23,5 +24,7 @@ public class OrderListItem {
         return serial;
     }
 
-
+    public String getStatus() {
+        return status;
+    }
 }
--- app/src/main/java/com/example/myapplication/OrderListItemAdapter.java
+++ app/src/main/java/com/example/myapplication/OrderListItemAdapter.java
@@ -22,6 +22,7 @@ public class OrderListItemAdapter extends FirestoreRecyclerAdapter<OrderListItem
 
 
     private static final int CRITICAL_PRICE = 45;
+    private static final int MAX_PRICE = 70;
     private final Context context;
 
     public OrderListItemAdapter(@NonNull FirestoreRecyclerOptions<OrderListItem> options, Context context) {
@@ -34,22 +35,54 @@ public class OrderListItemAdapter extends FirestoreRecyclerAdapter<OrderListItem
     protected void onBindViewHolder(@NonNull final OrderListItemHolder holder, int position, @NonNull final OrderListItem model) {
         holder.textViewTitle.setText(model.getSerial());//TODO - change to normal title
 
-
         progressBarHandler(holder, model);
 
+        priceTextHandler(holder, model);
 
+        statusTextHandler(holder, model);
+
+    }
+
+    private void statusTextHandler(OrderListItemHolder holder, OrderListItem model) {
+        if (model.getStatus().equals("open")) {
+            if (model.getPrice() > CRITICAL_PRICE) {
+                holder.statusText.setText("סטטוס: מוכנה לשילוח!");
+            }
+            else{
+                holder.statusText.setText("סטטוס: מחכה למשבחים...");
+            }
+        }
+        else if(model.getStatus().equals("locked")){
+            lock_order(holder);
+
+        }
+
+    }
+
+    private void lock_order(OrderListItemHolder holder) {
+        holder.statusText.setText("ההזמנה יצאה");
+        holder.joinButton.setText("נעול");
+        holder.joinButton.setBackgroundColor(context.getResources().getColor(R.color.grey));
+        holder.progressBar.setProgressDrawable(context.getDrawable(R.drawable.progress_bar_locked));    }
+
+    private void priceTextHandler(OrderListItemHolder holder, OrderListItem model) {
+        String priceStr = Integer.toString(model.getPrice());
+        String priceTextInput = String.format("הכסף שנצבר: %s מתוך 70 שקלים", priceStr);
+        holder.priceText.setText(priceTextInput);
 
     }
 
     private void progressBarHandler(@NonNull OrderListItemHolder holder, OrderListItem model) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             holder.progressBar.setProgress(model.getPrice(), TRUE);
-            if(model.getPrice()>70){
+            if (model.getPrice() > 70) {
                 holder.progressBar.setProgress(70, TRUE);
+
             }
         }
         if (model.getPrice() > CRITICAL_PRICE) {
             holder.progressBar.setProgressDrawable(context.getDrawable(R.drawable.progress_bar_green));
+
         } else {
             holder.progressBar.setProgressDrawable(context.getDrawable(R.drawable.progress_bar_orange));
 
@@ -67,13 +100,20 @@ public class OrderListItemAdapter extends FirestoreRecyclerAdapter<OrderListItem
     class OrderListItemHolder extends RecyclerView.ViewHolder {
         TextView textViewTitle;
         Button infoButton;
+        Button joinButton;
         ProgressBar progressBar;
+        TextView statusText;
+        TextView priceText;
+
 
         public OrderListItemHolder(View itemView) {
             super(itemView);
             textViewTitle = itemView.findViewById(R.id.order_title);
             infoButton = itemView.findViewById(R.id.btn_info);
+            joinButton = itemView.findViewById(R.id.btn_join);
             progressBar = itemView.findViewById(R.id.order_progress);
+            priceText = itemView.findViewById(R.id.money_text);
+            statusText = itemView.findViewById(R.id.status);
         }
     }
 
