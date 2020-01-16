4b90b79af35ebaeeb0d31ee6fdac3a600db9f4ed
Liav <liav25@gmail.com>
2020-01-16 17:28:52 +0200

    added clickable cardView for order recyclerView bind holder

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     //adapters
     private OrderListItemAdapter orderAdapter;
 
+    FloatingActionButton newOrderButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         connectToXML();
         setUpOrdersRecyclerView();
+        setFab();
 
     }
 
@@ -56,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * this function is calling the dialog
+     *
      * @param view - the button that his onClick call the function
      */
     public void launchDialog(View view) {
@@ -70,14 +85,15 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * this application starts the myBag Recycler View
+     *
      * @param myBagDialog parent dialog popup windows
      */
     private void setUpMyBag(Dialog myBagDialog) {
-       //this is an example query: "luli" is just a generic name for checking the filtering from
+        //this is an example query: "luli" is just a generic name for checking the filtering from
         //collections
         //Todo: change luli to getUserId after merging
         Query query = db.collectionGroup(getString(R.string.manot_collection))
-                .whereEqualTo(getString(R.string.owner_field),"luli");
+                .whereEqualTo(getString(R.string.owner_field), "luli");
 
         FirestoreRecyclerOptions options = new FirestoreRecyclerOptions.Builder<Mana>()
                 .setQuery(query, Mana.class)
@@ -105,6 +121,18 @@ public class MainActivity extends AppCompatActivity {
     private void connectToXML() {
         //views
         ImageView bag = findViewById(R.id.bag);
+        newOrderButt = findViewById(R.id.new_order_button);
+
+
+    }
+
+    private void setFab() {
+        newOrderButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewOrder();
+            }
+        });
     }
 
     @Override
@@ -117,6 +145,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         orderAdapter.stopListening();
+    }
+
+    private void createNewOrder() {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                // TODO Auto-generated method stub
+                addOrdertoServer();
+//                Intent intent = new Intent(MainActivity.this, ManaPickerActivity.class);
+//                startActivity(intent);
+            }
+        }, hour, minute, true);
+        timePickerDialog.show();
+    }
+
+    private void addOrdertoServer() {
+        final OrderListItem order = new OrderListItem("serial",0,"open");
+        ordersRef.add(order).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                task.getResult().collection("Manot").add(new Mana("avnush",0,10));
+            }
+        });
+
+
+
+
     }
 }
 
