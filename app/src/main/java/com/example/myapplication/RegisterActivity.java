20fd025255c5407330118e15a4cded52c5e505f0
eyaldiskin <eyal.diskin@mail.huji.ac.il>
2020-01-18 23:20:28 +0200

    split login activity into login and register activities

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -3,9 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -18,16 +16,16 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    EditText passwordText, emailText;
+    EditText nameText, passwordText, emailText;
     private FirebaseAuth firebaseAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -35,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void assignViewsFromLayout() {
+        nameText = findViewById(R.id.username_text);
         passwordText = findViewById(R.id.password_text);
         emailText = findViewById(R.id.email_text);
     }
@@ -42,56 +41,36 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-
-        //todo sign in?
     }
 
 
-    public void login(View view) {
-
-        // todo error handling
+    public void register(View view) {
+        final String username = nameText.getText().toString();
         final String emailAddress = emailText.getText().toString();
         final String password = passwordText.getText().toString();
-        final LoginActivity thisActivity = this;
-        firebaseAuth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(this,
+        firebaseAuth.createUserWithEmailAndPassword(emailAddress, password).addOnCompleteListener(this,
                 new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "Login successful!",
+                            Toast.makeText(RegisterActivity.this, "Register successful!",
                                     Toast.LENGTH_SHORT).show();
-                            Handler handler = new Handler();
+                            finish();
 
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Intent i = new Intent(thisActivity, MainActivity.class);
-                                    startActivity(i);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Problem while Registering",
+                                    Toast.LENGTH_SHORT).show();
 
-                                }
-                            },1200);
-                        }
-                        else {
-
-                            // display error in
                         }
                     }
+
                 });
     }
-
 
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        // todo sign out?
-    }
-
-    public void register(View view) {
-        Intent i = new Intent(this,RegisterActivity.class);
-        startActivityForResult(i,1); //todo - pick better request code
     }
 }
