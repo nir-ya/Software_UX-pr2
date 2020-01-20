a0be5b0e0bb00c5fd4583688d9c95579d2f7aefa
nir_ya <nir.yazdi@mail.huji.ac.il>
2020-01-20 16:46:33 +0200

    extract data members from intent

--- app/src/main/java/com/example/myapplication/OrderConfirmationActivity.java
+++ app/src/main/java/com/example/myapplication/OrderConfirmationActivity.java
@@ -3,9 +3,15 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class OrderConfirmationActivity extends AppCompatActivity {
+
+    String paymentMethod, manaType, orderId, orderTime;
+    HashMap<String, Boolean> tosafot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,23 @@ public class OrderConfirmationActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
 
         orderDetails.setText("הזמנה לשעה " + extras.getString("order_time"));
+
+        manaType = extras.getString("mana_type");
+        orderId = extras.getString("order_id");
+        orderTime = extras.getString("order_time");
+
+        tosafot = (HashMap<String, Boolean>) extras.getSerializable("tosafot");
     }
+
+    public void setCreditPayment(View view) {
+        paymentMethod = "Credit";
+    }
+
+
+    public void setCashPayment(View view) {
+        paymentMethod = "Cash";
+    }
+
+
+
 }
