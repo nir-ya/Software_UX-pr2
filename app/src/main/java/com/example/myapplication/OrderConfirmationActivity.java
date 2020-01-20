1f6ca421c2fc97f3ff8abd0518da6f43e3c4450e
nir_ya <nir.yazdi@mail.huji.ac.il>
2020-01-20 16:34:51 +0200

    added order time display to Order Confirmation Activity

--- app/src/main/java/com/example/myapplication/OrderConfirmationActivity.java
+++ app/src/main/java/com/example/myapplication/OrderConfirmationActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderConfirmationActivity extends AppCompatActivity {
 
@@ -11,10 +12,10 @@ public class OrderConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
 
-
+        TextView orderDetails = findViewById(R.id.order_details_text);
 
         Bundle extras = getIntent().getExtras();
 
-
+        orderDetails.setText("הזמנה לשעה " + extras.getString("order_time"));
     }
 }
