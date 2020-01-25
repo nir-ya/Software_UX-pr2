467b0cc02028d7e8a5c6d0a8c1053c8567e65f1a
Ran Hadar <ran.hadar@gmail.com>
2020-01-25 16:19:52 +0200

    Layout Fixes

--- app/src/main/java/com/example/myapplication/MyBagAdapter.java
+++ app/src/main/java/com/example/myapplication/MyBagAdapter.java
@@ -16,6 +16,8 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 import android.view.View;
 
+import org.w3c.dom.Text;
+
 public class MyBagAdapter extends FirestoreRecyclerAdapter<Mana, MyBagAdapter.MyBagHolder> {
 
 
@@ -34,6 +36,13 @@ public class MyBagAdapter extends FirestoreRecyclerAdapter<Mana, MyBagAdapter.My
         holder.textViewType.setText(mana.getHebType(mana.getType()));
         holder.textViewPrice.setText(Integer.toString(mana.getPrice()));
         holder.tosafot.setText(mana.getTosafotString());
+        holder.statusText.setText(mana.getStatus());
+        if(mana.getPaymentMethod()==Mana.MEZUMAN){
+            holder.paymentImg.setImageDrawable(context.getDrawable(R.drawable.cash));
+        }
+        else {
+            holder.paymentImg.setImageDrawable(context.getDrawable(R.drawable.credit));
+        }
     }
 
 
@@ -50,11 +59,18 @@ public class MyBagAdapter extends FirestoreRecyclerAdapter<Mana, MyBagAdapter.My
         TextView textViewType;
         TextView textViewPrice;
         TextView tosafot;
+        TextView hourText;
+        TextView statusText;
+        ImageView paymentImg;
+
         public MyBagHolder(View itemView) {
             super(itemView);
             textViewType = itemView.findViewById(R.id.manaTypeTxt);
             textViewPrice = itemView.findViewById(R.id.manaPriceTxt);
             tosafot = itemView.findViewById(R.id.tosafutTxt);
+            hourText = itemView.findViewById(R.id.hourTxt);
+            statusText = itemView.findViewById(R.id.statusTxt);
+            paymentImg = itemView.findViewById(R.id.paymentImg);
         }
     }
 
