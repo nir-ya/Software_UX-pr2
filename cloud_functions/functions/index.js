85e9325e697a148e57986095be7d796f9868821f
eyaldiskin <eyal.diskin@mail.huji.ac.il>
2020-02-27 18:45:32 +0200

    added timestamp and updated status of manot

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -18,7 +18,7 @@ exports.updatePriceOnManaChange = functions.firestore.document("OpenOrders/{orde
     var orderRef = db.collection('OpenOrders').doc(context.params.orderId);
     return db.runTransaction(transaction => {
         return transaction.get(orderRef).then(orderDoc => {
-            var newPrice = orderDoc.data().price  + manaAfterPrice - manaBeforePrice;
+            var newPrice = orderDoc.data().price + manaAfterPrice - manaBeforePrice;
             return transaction.update(orderRef, {
                 price: newPrice
             });
@@ -33,9 +33,15 @@ exports.updatePriceOnManaChange = functions.firestore.document("OpenOrders/{orde
 exports.updatePriceOnManaCreate = functions.firestore.document("OpenOrders/{orderId}/Manot/{manaId}").onCreate((snapshot, context) => {
     var manaPrice = snapshot.data().price;
     var orderRef = db.collection('OpenOrders').doc(context.params.orderId);
+
     return db.runTransaction(transaction => {
         return transaction.get(orderRef).then(orderDoc => {
-            var newPrice = orderDoc.data().price  + manaPrice;
+            //var timestamp = orderDoc.data().timestamp;
+
+            var newPrice = orderDoc.data().price + manaPrice;
+
+
+
             return transaction.update(orderRef, {
                 price: newPrice
             });
@@ -44,19 +50,62 @@ exports.updatePriceOnManaCreate = functions.firestore.document("OpenOrders/{orde
 
 });
 
+exports.updateTimestampOnManaCreation = functions.firestore.document("OpenOrders/{orderId}/Manot/{manaId}").onCreate((snapshot, context) => {
+    var orderRef = db.collection('OpenOrders').doc(context.params.orderId);
+
+    return db.runTransaction(transaction => {
+        return transaction.get(orderRef).then(orderDoc => {
+            var timestamp = orderDoc.data().timestamp;
+            var manaRef = db.collection('OpenOrders').doc(context.params.orderId).collection("Manot").doc(context.params.manaId)
+
+            return transaction.update(manaRef, {
+                timestamp: timestamp
+            });
+        });
+    });
+
+});
+
+exports.updateManaStatusOnOrderStatusChange = functions.firestore.document("OpenOrders/{orderId}").onUpdate((change, context) => {
+    var orderBeforeStatus = change.before.data().status;
+    var orderAfterStatus = change.after.data().status;
+    var orderId = context.params.orderId;
+    if ((orderBeforeStatus === "open") && (orderAfterStatus === "closed" || orderAfterStatus === "canceled")) {
+        return admin.firestore().collection('/OpenOrders/' + orderId + '/Manot')
+            .get().then(snapshot => {
+                const promises = [];
+                snapshot.forEach(doc => {
+                    promises.push(doc.ref.update({
+                        status: orderAfterStatus
+                    }));
+                });
+                return Promise.all(promises)
+            })
+            .catch(error => {
+                console.log(error);
+                return null;
+            });
+    }
+
+
+
+
+});
+
+
 /**
  * update order price on mana deletion
  */
-exports.updatePriceOnManaDelete = functions.firestore.document("OpenOrders/{orderId}/Manot/{manaId}").onDelete((snapshot,context) => {
+exports.updatePriceOnManaDelete = functions.firestore.document("OpenOrders/{orderId}/Manot/{manaId}").onDelete((snapshot, context) => {
     var manaPrice = snapshot.data().price;
     var orderRef = db.collection('OpenOrders').doc(context.params.orderId);
     return db.runTransaction(transaction => {
         return transaction.get(orderRef).then(orderDoc => {
-            var newPrice = orderDoc.data().price  - manaPrice;
-            if(newPrice === 0){
+            var newPrice = orderDoc.data().price - manaPrice;
+            if (newPrice === 0) {
                 return transaction.delete(orderRef);
             }
-            else{
+            else {
                 return transaction.update(orderRef, {
                     price: newPrice
                 });
