f0aeecd9c0ac0398c8b9fb072382fe554a9b43a8
Liav <liav25@gmail.com>
2020-03-01 01:09:44 +0200

    add more sounds

--- app/src/main/java/com/example/myapplication/Randomizer.java
+++ app/src/main/java/com/example/myapplication/Randomizer.java
@@ -5,39 +5,28 @@ import java.text.Format;
 import java.text.SimpleDateFormat;
 
 public class Randomizer {
-    public static int generate(int min,int max) {
-        return min + (int)(Math.random() * ((max - min) + 1));
+    private static int generate(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
     }
 
     static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     static SecureRandom rnd = new SecureRandom();
 
-    public static String randomString( int len ){
-        StringBuilder sb = new StringBuilder( len );
-        for( int i = 0; i < len; i++ )
-            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+    static String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
         return sb.toString();
     }
 
-    public static Format formatter = new SimpleDateFormat("HH:mm");
+    static Format formatter = new SimpleDateFormat("HH:mm");
 
-    public static int randomSadSound(){
-        int rand = generate(0,5);
-        switch (rand){
-            case 0:
-                return R.raw.basa_sound;
-            case 1:
-                return R.raw.basa_whitney;
-            case 2:
-                return R.raw.letitgoo;
-            case 3:
-                return R.raw.sad_trombone;
-            case 4:
-                return R.raw.believefly_basa;
-            case 5:
-                return R.raw.titanic_basa;
-        }
-        return R.raw.basa_whitney;
+
+    int[] sadSounds = new int[]{R.raw.basa_whitney, R.raw.basa_sound,
+            R.raw.sad_trombone, R.raw.letitgoo, R.raw.believefly_basa, R.raw.titanic};
+
+    static int randomSadSound() {
+        return Constants.SAD_SOUNDS[generate(0,5)];
     }
 
 }
